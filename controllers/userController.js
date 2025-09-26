edc91215dba846a8987091ddf3a61c5a94651db0
Viswajith2003 <viswajithviswa715@gmail.com>
2025-09-26 13:37:07 +0530

    data visible in db

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,32 @@ const loadRegister = async (req, res) => {
   }
 };
 
+const insertUser = async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      mobile: req.body.mobile,
+      password: req.body.password,
+      image: req.file.filename,
+      is_admin: 0,
+    });
+
+    const userData = await user.save();
+    if (userData) {
+      res.render("registration", {
+        message: "Your registration has been successed",
+      });
+    } else {
+      res.render("registration", {
+        message: "Your registration has been failed",
+      });
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
 module.exports = {
-  loadRegister
+  loadRegister,insertUser
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,32 @@
-const express=require('express');
-const user_route=express();
+const express = require("express");
+const user_route = express();
 
-user_route.set('view engine','ejs');
-user_route.set('views','./views/users')
+user_route.set("view engine", "ejs");
+user_route.set("views","./views/users");
 
-const userController=require("../controllers/userController")
+const bodyParser = require("body-parser");
+user_route.use(bodyParser.json());
+user_route.use(bodyParser.urlencoded({ extended: true }));
 
-user_route.get("/register",userController.loadRegister)
+const multer = require("multer");
+const path = require("path");
 
-module.exports=user_route;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/userImages"));
+  },
+  filename: function (req, file, cb) {
+    const name = Date.now() + "-" + file.originalname;
+    cb(null, name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const userController = require("../controllers/userController");
+
+user_route.get("/register", userController.loadRegister);
+
+user_route.post("/register", upload.single("image"), userController.insertUser);
+
+module.exports = user_route;
